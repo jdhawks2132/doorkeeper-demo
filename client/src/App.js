75dfe9a745fbfc760666350c679c64362d5c1e27
snl95 <snl95@txstate.edu>
2022-04-21 14:45:59 -0500

    added navbar and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Hello from App</h1>
+			<ResponsiveAppBar />
 		</div>
 	);
 }
